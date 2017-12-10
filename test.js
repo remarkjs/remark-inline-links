73d632690b89ee56252069f1b1041db9dafe105d
Titus Wormer <tituswormer@gmail.com>
2017-12-10 12:05:32 +0100

    Refactor tests

--- test.js
+++ test.js
@@ -5,37 +5,44 @@ var remark = require('remark');
 var inlineLinks = require('./');
 
 test('remark-inline-links', function (t) {
-  remark().use(inlineLinks).process([
-    '[foo], [foo][], [bar][foo].',
-    '',
-    '![foo], ![foo][], ![bar][foo].',
-    '',
-    '[baz], [baz][], [bar][baz].',
-    '',
-    '![baz], ![baz][], ![bar][baz].',
-    '',
-    '[foo]: http://example.com "Example Domain"',
-    '',
-    '[qux]: http://example.com#qux "Qux"',
-    ''
-  ].join('\n'), function (err, file) {
-    t.ifErr(err);
+  t.plan(1);
 
-    t.equal(file.toString(), [
-      '[foo](http://example.com "Example Domain"), ' +
-        '[foo](http://example.com "Example Domain"), ' +
-        '[bar](http://example.com "Example Domain").',
+  remark()
+    .use(inlineLinks)
+    .process([
+      '[foo], [foo][], [bar][foo].',
       '',
-      '![foo](http://example.com "Example Domain"), ' +
-        '![foo](http://example.com "Example Domain"), ' +
-        '![bar](http://example.com "Example Domain").',
+      '![foo], ![foo][], ![bar][foo].',
       '',
       '[baz], [baz][], [bar][baz].',
       '',
       '![baz], ![baz][], ![bar][baz].',
+      '',
+      '[foo]: http://example.com "Example Domain"',
+      '',
+      '[qux]: http://example.com#qux "Qux"',
       ''
-    ].join('\n'));
-
-    t.end();
-  });
+    ].join('\n'), function (err, file) {
+      t.deepEqual(
+        [err, String(file)],
+        [
+          null,
+          [
+            '[foo](http://example.com "Example Domain"), ' +
+              '[foo](http://example.com "Example Domain"), ' +
+              '[bar](http://example.com "Example Domain").',
+            '',
+            '![foo](http://example.com "Example Domain"), ' +
+              '![foo](http://example.com "Example Domain"), ' +
+              '![bar](http://example.com "Example Domain").',
+            '',
+            '[baz], [baz][], [bar][baz].',
+            '',
+            '![baz], ![baz][], ![bar][baz].',
+            ''
+          ].join('\n')
+        ],
+        'should process'
+      );
+    });
 });
